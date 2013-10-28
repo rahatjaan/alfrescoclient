5a4c8a992fd3dd79caa96784cf9be71ccd0a2cdb
Rahat Ali <rahat.jaan@gmail.com>
2013-10-28 13:32:23 +0500

    Downoad Content

--- src/main/java/com/servercenter/alfresco/client/wrapper/ContentWrapper.java
+++ src/main/java/com/servercenter/alfresco/client/wrapper/ContentWrapper.java
@@ -2,12 +2,16 @@ package com.servercenter.alfresco.client.wrapper;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.rmi.RemoteException;
 
 import org.alfresco.webservice.authentication.AuthenticationFault;
 import org.alfresco.webservice.content.Content;
+import org.alfresco.webservice.content.ContentFault;
+import org.alfresco.webservice.content.ContentServiceSoapBindingStub;
 import org.alfresco.webservice.repository.RepositoryFault;
 import org.alfresco.webservice.repository.UpdateResult;
 import org.alfresco.webservice.types.CML;
@@ -15,6 +19,7 @@ import org.alfresco.webservice.types.CMLCreate;
 import org.alfresco.webservice.types.ContentFormat;
 import org.alfresco.webservice.types.NamedValue;
 import org.alfresco.webservice.types.ParentReference;
+import org.alfresco.webservice.types.Predicate;
 import org.alfresco.webservice.types.Reference;
 import org.alfresco.webservice.util.Constants;
 import org.alfresco.webservice.util.ContentUtils;
@@ -28,9 +33,21 @@ public class ContentWrapper extends WrapperBase{
 //		InputStream viewStream = contentWrapper.getClass().getClassLoader().getResourceAsStream("org/alfresco/webservice/test/resources/test.jpg");
 //		Reference contentRef = contentWrapper.createContent(viewStream,"rahat_content","28d1e2c1-43ef-449b-b0b6-efb691fbc8b5","image/jpeg");
 //		System.out.println("Content uuid:"+contentRef.getUuid());
-		File f = new File("d://rahat.txt");
+		File f = new File("d://rahat2.txt");
 		Reference contentRef = contentWrapper.createContent(f,"28d1e2c1-43ef-449b-b0b6-efb691fbc8b5","txt");
 		System.out.println("Content uuid:"+contentRef.getUuid());
+		Content content = contentWrapper.getContent(contentRef.getUuid(), null);
+		System.out.println("Content Received:"+ContentUtils.getContentAsString(content));
+		InputStream stream = ContentUtils.getContentAsInputStream(content);
+		OutputStream output= new FileOutputStream(new File("d://rahat_output.txt"));
+		int read = 0;
+		byte[] bytes = new byte[1024];
+ 
+		while ((read = stream.read(bytes)) != -1) {
+			output.write(bytes, 0, read);
+		}
+		stream.close();
+		output.close();
 	}
 
 	public Reference createContent(File input,String parentUuid,String mimeType) throws IOException {
@@ -40,6 +57,22 @@ public class ContentWrapper extends WrapperBase{
 
 		return createContent(is,input.getName(),parentUuid,mimeType);
 //		return null;
+	}
+	
+	public Content getContent(String contentUuid,String path) throws ContentFault, RemoteException{
+		try {
+			startSession();
+		} catch (AuthenticationFault e1) {
+			e1.printStackTrace();
+		}
+		ContentServiceSoapBindingStub contentService = WebServiceFactory.getContentService();
+		 Reference newContentReference = new Reference(STORE, contentUuid, path);
+		Content[] readResult2 = contentService.read(
+                 new Predicate(new Reference[]{newContentReference}, STORE, null), 
+                 Constants.PROP_CONTENT);
+		 Content content = readResult2[0];
+		 endSession();
+		 return content;
 	}
 	public Reference createContent(InputStream input,String contentName,String parentUuid,String mimeType) throws RepositoryFault, RemoteException {
 		try {
